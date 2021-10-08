8e6d9cbf95cb895cd69fc55d08da317693de6aee
Keith Winstein <keithw@cs.stanford.edu>
2021-10-08 13:49:16 -0700

    Add layer.hh

---
+++ src/nn/layer.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <array>
+
+template<unsigned int input_size, unsigned int output_size>
+class Layer
+{
+  std::array<float, output_size> activations_;
+
+public:
+  void apply( const std::array<float, input_size>& input );
+
+  const std::array<float, output_size>& activations() const { return activations_; }
+};
