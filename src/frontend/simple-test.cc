62a8b6d0b59d9da74b7484a453c1536330bd835b
Jamie Song <jamiesxy@stanford.edu>
2022-02-02 16:26:50 -0800

    make format

--- src/frontend/simple-test.cc
+++ src/frontend/simple-test.cc
@@ -4,8 +4,8 @@
 
 #include <Eigen/Dense>
 #include <iostream>
-#include <utility>
 #include <random>
+#include <utility>
 
 #include <sys/resource.h>
 #include <sys/time.h>
@@ -17,15 +17,17 @@ constexpr size_t batch_size = 1;
 constexpr size_t input_size = 1;
 
 vector<Matrix<float, batch_size, input_size>> input {};
-vector<Matrix<float, batch_size, input_size>> output{};
+vector<Matrix<float, batch_size, input_size>> output {};
 
-float getLoss(float expect, float nnOutput) {
-  return (expect - nnOutput) * (expect - nnOutput);
+float getLoss( float expect, float nnOutput )
+{
+  return ( expect - nnOutput ) * ( expect - nnOutput );
 }
 
-float get_pd_loss_output(float expect, float nnOutput) {
+float get_pd_loss_output( float expect, float nnOutput )
+{
   cout << "inside: " << expect << " " << nnOutput << endl;
-  return -2 * (expect - nnOutput);
+  return -2 * ( expect - nnOutput );
 }
 
 void program_body()
@@ -41,31 +43,31 @@ void program_body()
   auto nn = make_unique<Network<float, batch_size, input_size, 1>>();
   nn->layer0.weights()( 0 ) = 1;
   nn->layer0.biases()( 0 ) = 1;
-  
+
   /* input and output */
-  for (int i = 0; i < 10; i++) {
+  for ( int i = 0; i < 10; i++ ) {
     Matrix<float, batch_size, input_size> in, out;
     int v = rand() % 100 + 1;
-    in(0) = (float)v;
-    out(0) = 3 * v + 1 + (float)rand()/(RAND_MAX);
-    input.push_back(in);
-    output.push_back(out);
+    in( 0 ) = (float)v;
+    out( 0 ) = 3 * v + 1 + (float)rand() / ( RAND_MAX );
+    input.push_back( in );
+    output.push_back( out );
   }
-  for (int round = 0; round < 100; round++) {
-    for (int i = 0; i < 10; i++) {
+  for ( int round = 0; round < 100; round++ ) {
+    for ( int i = 0; i < 10; i++ ) {
       nn->apply( input[i] );
-      cout << input[i](0) << "  " << output[i](0) << endl;
+      cout << "right after apply: " << input[i]( 0 ) << "  " << output[i]( 0 ) << endl;
 
-      //cout << "output: " << nn->output() << "\n";
+      // cout << "output: " << nn->output() << "\n";
 
-      //const float loss = ( 100 - nn->output()( 0 ) ) * ( 100 - nn->output()( 0 ) );
-      const float loss = getLoss(output[i](0), nn->output()(0));
+      // const float loss = ( 100 - nn->output()( 0 ) ) * ( 100 - nn->output()( 0 ) );
+      const float loss = getLoss( output[i]( 0 ), nn->output()( 0 ) );
 
       nn->computeDeltas();
       nn->evaluateGradients( input[i] );
 
-      //const float pd_loss_output = -2 * ( 100 - nn->output()( 0 ) );
-      const float pd_loss_output = get_pd_loss_output(output[i](0), nn->output()(0));
+      // const float pd_loss_output = -2 * ( 100 - nn->output()( 0 ) );
+      const float pd_loss_output = get_pd_loss_output( output[i]( 0 ), nn->output()( 0 ) );
 
       cout << "partial derivative of output wrt weight = " << nn->getEvaluatedGradient( 0, 0 ) << "\n";
       cout << "partial derivative of output wrt bias = " << nn->getEvaluatedGradient( 0, 1 ) << "\n";
@@ -74,10 +76,11 @@ void program_body()
 
       cout << "partial derivative of loss wrt output = " << pd_loss_output << "\n";
 
-      cout << "partial derivative of loss wrt weight = " << pd_loss_output * nn->getEvaluatedGradient( 0, 0 ) << "\n";
+      cout << "partial derivative of loss wrt weight = " << pd_loss_output * nn->getEvaluatedGradient( 0, 0 )
+           << "\n";
       cout << "partial derivative of loss wrt bias = " << pd_loss_output * nn->getEvaluatedGradient( 0, 1 ) << "\n";
-      cout << "weight = " << nn->layer0.weights()(0) << endl;
-      cout << "biase = " << nn->layer0.biases()(0) << endl;
+      cout << "weight = " << nn->layer0.weights()( 0 ) << endl;
+      cout << "biase = " << nn->layer0.biases()( 0 ) << endl;
 
       const float learning_rate = .0001;
 
@@ -85,17 +88,17 @@ void program_body()
       nn->layer0.biases()( 0 ) *= ( 1 - learning_rate * pd_loss_output * nn->getEvaluatedGradient( 0, 1 ) );
     }
   }
- 
+
   // testing
   cout << "start testing results" << endl;
   Matrix<float, batch_size, input_size> test_input, test_output;
-  for (int i = 0; i < 10; i++) {
+  for ( int i = 0; i < 10; i++ ) {
     int v = rand() % 100 + 1;
-    test_input(0) = (float)v;
-    test_output(0) = 3 * v + 1;
-    cout << "input: " << test_input(0) << ", output: " << test_output(0) << endl;
-    nn -> apply( test_input );
-    auto nnOutput = nn -> output();
+    test_input( 0 ) = (float)v;
+    test_output( 0 ) = 3 * v + 1;
+    cout << "input: " << test_input( 0 ) << ", output: " << test_output( 0 ) << endl;
+    nn->apply( test_input );
+    auto nnOutput = nn->output();
     cout << "nn output: " << nnOutput << endl;
   }
 }
