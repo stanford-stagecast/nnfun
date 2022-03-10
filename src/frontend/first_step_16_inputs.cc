5a8a289b0a598612e092715f065870501b3bb7ad
Jamie Song <jamiesxy@stanford.edu>
2022-03-09 16:38:35 -0800

    make format

--- src/frontend/first_step_16_inputs.cc
+++ src/frontend/first_step_16_inputs.cc
@@ -50,44 +50,27 @@ void program_body()
   nn->layer0.initializeWeightsRandomly();
 
   /* test true function */
-  //Matrix<float, input_size, 1> input(16);
-  //input << 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16;
-  //cout << true_function(input) << endl;
-  for (auto i = 0; i < 15; i++) {
+  // Matrix<float, input_size, 1> input(16);
+  // input << 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16;
+  // cout << true_function(input) << endl;
+  for ( auto i = 0; i < 15; i++ ) {
     /* step 1: construct a unique problem instance */
     Matrix<float, batch_size, input_size> input, ground_truth_output;
 
-    input << 1+i,2+i,3+i,4+i,5+i,6+i,7+i,8+i,9+i,10+i,11+i,12+i,13+i,14+i,15+i,16+i;
-    ground_truth_output(0, 0) = true_function(input);
+    input << 1 + i, 2 + i, 3 + i, 4 + i, 5 + i, 6 + i, 7 + i, 8 + i, 9 + i, 10 + i, 11 + i, 12 + i, 13 + i, 14 + i,
+      15 + i, 16 + i;
+    ground_truth_output( 0, 0 ) = true_function( input );
 
-    cout << "problem instance: from " << input(0,0) << " to " << input(0,15) << " => " << ground_truth_output(0,0) << endl;
+    cout << "problem instance: from " << input( 0, 0 ) << " to " << input( 0, 15 ) << " => "
+         << ground_truth_output( 0, 0 ) << endl;
 
     /* step 2: forward propagate and calculate loss functiom */
     nn->apply( input );
-    cout << "nn maps input: " << input(0,0) << " to " << input(0,15) << " => " << nn->output()(0,0) << endl;
+    cout << "nn maps input: " << input( 0, 0 ) << " to " << input( 0, 15 ) << " => " << nn->output()( 0, 0 )
+         << endl;
 
-    cout << "loss: " << loss_function (nn->output()(0,0), ground_truth_output(0,0)) << endl;
+    cout << "loss: " << loss_function( nn->output()( 0, 0 ), ground_truth_output( 0, 0 ) ) << endl;
 
-    /* step 3: backpropagate error */
-    nn->computeDeltas();
-    nn->evaluateGradients( input );
-
-    const float pd_loss_wrt_output = compute_pd_loss_wrt_output( ground_truth_output(0,0), nn->output()(0,0) );
-
-    // TODO: static eta -> dynamic eta
-	learning_rate = 0.001;
-    float loss = loss_function(nn->output()(0,0), ground_truth_output(0,0));
-
-    for (int j = 0; j < 16; j++) {
-      nn->layer0.weights()(0) -= learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient(0,j);
-    }
-    nn->layer0.biases()(0) -= learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient(0,16);
-
-    cout << pd_loss_wrt_output << " " << loss << endl;
-  }
-
-#if 0
-  for ( auto i = 0; i < 1000; i++ ) {
     /* step 3: backpropagate error */
     nn->computeDeltas();
     nn->evaluateGradients( input );
@@ -95,6 +78,21 @@ void program_body()
     const float pd_loss_wrt_output
       = compute_pd_loss_wrt_output( ground_truth_output( 0, 0 ), nn->output()( 0, 0 ) );
 
+    // TODO: static eta -> dynamic eta
+    learning_rate = 0.001;
+    float loss = loss_function( nn->output()( 0, 0 ), ground_truth_output( 0, 0 ) );
+
+    for ( int j = 0; j < 16; j++ ) {
+      nn->layer0.weights()( 0 ) -= learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient( 0, j );
+    }
+    nn->layer0.biases()( 0 ) -= learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient( 0, 16 );
+
+    cout << pd_loss_wrt_output << " " << loss << endl;
+  }
+
+#if 0
+  for ( auto i = 0; i < 1000; i++ ) {
+    /* step 3: backpropagate error */
     auto temp_learning_rate_one = 4.0 / 3 * learning_rate;
     auto temp_learning_rate_two = 2.0 / 3 * learning_rate;
 
