1000a39812d872ad534149b9889b39592112d153
Jamie Song <jamiesxy@stanford.edu>
2022-05-09 19:46:06 -0700

    multiple iterations

--- src/frontend/multi_layer_nn_template.cc
+++ src/frontend/multi_layer_nn_template.cc
@@ -28,12 +28,13 @@ float true_function( const float input )
   return 1.0 / input;
 }
 
-float learning_rate = 100;
+float learning_rate = 0.001;
 
 constexpr size_t batch_size = 1;
 constexpr size_t input_size = 1;
 constexpr size_t output_size = 1;
-vector<unsigned int> layers { input_size, 1, output_size };
+constexpr size_t num_iterations = 10;
+vector<unsigned int> layers { input_size, 10, 10, output_size };
 // TODO do not know how to put the vector into nn
 
 void program_body( Matrix<float, batch_size, input_size> input,
@@ -44,30 +45,32 @@ void program_body( Matrix<float, batch_size, input_size> input,
   CheckSystemCall( "setrlimit", setrlimit( RLIMIT_STACK, &limits ) );
 
   /* construct neural network on heap */
-  auto nn = make_unique<Network<float, batch_size, input_size, 1, output_size>>();
+  auto nn = make_unique<Network<float, batch_size, input_size, 10, 10, output_size>>();
 
   nn->initializeWeightsRandomly();
 
-  nn->apply( input );
+  for ( int iter = 0; iter < (int)num_iterations; iter++ ) {
+    nn->apply( input );
 
-  nn->print();
-  nn->computeDeltas();
-  nn->evaluateGradients( input );
+    nn->computeDeltas();
+    nn->evaluateGradients( input );
 
-  const float pd_loss_wrt_output = compute_pd_loss_wrt_output( ground_truth_output( 0, 0 ), nn->output()( 0, 0 ) );
-  cout << pd_loss_wrt_output << endl;
-  float loss = 0.0;
-  for ( int i = 0; i < (int)output_size; i++ ) {
-    loss += loss_function( nn->output()( 0, i ), ground_truth_output( 0, i ) );
-  }
+    const float pd_loss_wrt_output
+      = compute_pd_loss_wrt_output( ground_truth_output( 0, 0 ), nn->output()( 0, 0 ) );
+    //float loss = loss_function( nn->output()( 0, 0 ), ground_truth_output( 0, 0 ) );
 
-  for ( auto i = 0; i < (int)nn->getNumLayers(); i++ ) {
-    cout << "i: " << i << endl;
+    for ( auto i = 0; i < (int)nn->getNumLayers(); i++ ) {
+      //cout << "i: " << i << endl;
 
-    for ( auto j = 0; j < (int)nn->getNumParams( i ); j++ ) {
-      nn->modifyParam( (unsigned int)i, j, learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient( i, j ) );
+      for ( auto j = 0; j < (int)nn->getNumParams( i ); j++ ) {
+        nn->modifyParam(
+          (unsigned int)i, j, learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient( i, j ) );
+      }
     }
   }
+  input(0,0) = 3.0;
+  nn->apply(input);
+  cout << "result: " << nn->output()(0,0) << endl;
   nn->print();
 }
 
