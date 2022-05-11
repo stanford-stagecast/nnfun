1eb3a1dab8fbc344c00c6c28e7c1ed93927c72d4
Jamie Song <jamiesxy@stanford.edu>
2022-05-11 16:13:50 -0700

    class created

---
+++ src/nn/neuralnetwork.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <iostream>
+
+using namespace std;
+using namespace Eigen;
+
+template<class T, unsigned int batch_size, unsigned int num_layers, vector<unsigned int> layers>
+
+class NeuralNetwork
+{
+}
