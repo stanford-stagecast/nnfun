ec9a24f4f95892ca028d4a07d47e67365c1f4098
Jamie Song <jamiesxy@stanford.edu>
2022-05-18 02:19:22 -0700

    First step of abstraction. Manual described in docs. Issues reported in progress_report.

---
+++ src/frontend/test_new_class.cc
@@ -0,0 +1,70 @@
+#include "exception.hh"
+#include "neuralnetwork.hh"
+#include "timer.hh"
+
+#include <Eigen/Dense>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <utility>
+
+#include <sys/resource.h>
+
+using namespace std;
+using namespace Eigen;
+
+float loss_function( const float target, const float actual )
+{
+  return ( target - actual ) * ( target - actual );
+}
+
+float compute_pd_loss_wrt_output( const float target, const float actual )
+{
+  return -2 * ( target - actual );
+}
+
+float true_function( const float input )
+{
+  return 1.0 / input;
+}
+
+constexpr size_t batch_size = 1;
+constexpr size_t input_size = 16;
+constexpr size_t output_size = 1;
+
+void program_body(){
+  // num_layer = 2
+  // batch_size = 1
+  // input_size = 6
+  // output_size = 1
+  // 16 -> 16 -> 1
+  // 2 1 16 1 16 1
+  auto nn = make_unique<NeuralNetwork<float, 2, batch_size, input_size, output_size, 16, 1>>();
+  nn->initialize();
+  nn->print();
+
+  Matrix<float, batch_size, input_size> input;
+  Matrix<float, batch_size, output_size> ground_truth_output;
+  input = Matrix<float, 1, 16>::Random();
+  ground_truth_output << 40;
+  nn->apply(input);
+  cout << nn->get_output()(0,0) << endl; 
+  nn->gradient_descent(input, ground_truth_output, 0.0001);
+ 
+  nn->print();
+}
+
+
+
+int main( int argc, char*[] )
+{
+  try {
+    if ( argc <= 0 ) {
+      abort();
+    }
+    program_body();
+    return EXIT_SUCCESS;
+  } catch ( const exception& e ) {
+    return EXIT_FAILURE;
+  }
+}
--- src/nn/neuralnetwork.hh
+++ src/nn/neuralnetwork.hh
@@ -1,36 +1,22 @@
 #pragma once
+//#pragma GCC diagnostic push
+//#pragma GCC diagnostic ignored "-Werror=effc++"
 
 #include "network.hh"
 
 #include <Eigen/Dense>
-#include <iostream>
 
 using namespace std;
 using namespace Eigen;
 
-
+// note that output_size need to be input twice,
+// once as output_size, once in rest
+template<class T, unsigned int num_of_layers, unsigned int batch_size, unsigned int input_size, unsigned int output_size, unsigned int... rest>
 class NeuralNetwork
 {
-private:
-  unsigned int num_of_layers; // total numebr of layers (excluding output)
-                              // 1 -> 2 -> 4 -> 1     3 layers
-  unsigned int input_size;    // number of inputs to the nn
-  unsigned int output_size;   // number of outputs from the nn
-  unsigned int batch_size;
-  class T;
-  Network<T, batch_size, input_size, rest...> network;
 
 public:
-  // ctor
-  NeuralNetwork(class T, unsigned int b_size, unsigned int num_layers, 
-                vector<unsigned int> layers) {
-    cout << layers.size() << endl;
-    num_of_layers = num_layers;
-    batch_size = b_size;
-    input_size = layers.front();
-    output_size = layers.back();
-    network = make_unique<Network<float, batch_size, layers.front(), layers.back()>>();
-  }
+  Network<T, batch_size, input_size, rest...>* nn {};
 
   // getters
   unsigned int get_num_of_layers() {
@@ -44,4 +30,47 @@ public:
   unsigned int get_output_size() {
     return output_size;
   }
+
+  // default: initialize weights and biases randomly
+  void initialize() {
+    nn = new Network<T, batch_size, input_size, rest...>();
+    nn->initializeWeightsRandomly();
+  }
+
+  void print() {
+    nn->print();
+  }
+
+  void apply( const Matrix<T, batch_size, input_size>& input ) {
+    nn->apply(input);
+  }
+
+  const Matrix<T, batch_size, output_size>& get_output() {
+    return nn->output();
+  }
+
+  float compute_pd_loss_wrt_output( const float target, const float actual )
+  {
+    return -2 * ( target - actual );
+  }
+
+  void gradient_descent(Matrix<T, batch_size, input_size>& input, Matrix<T, batch_size, output_size>& ground_truth_output, float learning_rate) {
+    nn->apply(input);
+    nn->computeDeltas();
+    nn->evaluateGradients(input);
+
+    float pd_loss_wrt_output = 0;
+    // assuming batch_size = 1
+    for (int i = 0; i < (int)output_size; i++) {
+      pd_loss_wrt_output += compute_pd_loss_wrt_output(ground_truth_output(0,i), nn->output()(0,i));
+    }
+
+    // currently not dynamic learning rate
+    for (int i = 0; i < (int)nn->getNumLayers(); i++) {
+      for (int j = 0; j < (int)nn->getNumParams(i); j++) {
+        nn->modifyParam((unsigned int)i, j, learning_rate * pd_loss_wrt_output * nn->getEvaluatedGradient(i,j));
+      }
+    }
+  }
 };
+
