efc54d9cb8e883b9e3e8cca87b4c525d93594d6e
yasmitch <yasminem@stanford.edu>
2022-05-19 16:31:44 -0700

    Continuous prediction import bug fix

--- src/frontend/predict_tempo_midi_continuous.cc
+++ src/frontend/predict_tempo_midi_continuous.cc
@@ -4,28 +4,31 @@
 
   We want range of tempo: 35 - 250 bpm //TODO
 */
-#include "eventloop.hh"
 #include "exception.hh"
-#include "midi_processor.hh"
 #include "network.hh"
 #include "timer.hh"
+#include "midi_processor.hh"
 
 #include <Eigen/Dense>
 #include <iostream>
 #include <random>
 #include <utility>
+#include <queue>
+
+#include "eventloop.hh"
+
 
 #include <fcntl.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 
+
 using namespace std;
 using namespace Eigen;
 
 constexpr size_t batch_size = 1;
 constexpr size_t input_size = 16;
-constexpr unsigned int KEY_DOWN = 144;
 
 /* use squared error as loss function */
 float loss_function( const float target, const float actual )
@@ -44,12 +47,13 @@ Matrix<float, batch_size, input_size> gen_time( float tempo, float offset )
 {
   Matrix<float, batch_size, input_size> ret_mat;
   for ( auto i = 0; i < 16; i++ ) {
-    ret_mat( i ) = i/(tempo/60.0) + offset + 2 * ( (float)rand() ) / ( (float)RAND_MAX ) - 1;
+    ret_mat( i ) = (tempo) * i + offset;
   }
+  //cout << ret_mat << endl;
   return ret_mat;
 }
 
-float learning_rate = 0.00001;
+float learning_rate = 0.0001;
 
 void program_body( const string& midi_filename )
 {
@@ -63,22 +67,33 @@ void program_body( const string& midi_filename )
   /* construct neural network on heap */
   auto nn = make_unique<Network<float, batch_size, input_size, 1>>();
   nn->layer0.initializeWeightsRandomly();
-
-  int tempo = 50;
+  /*for ( int i = 0; i < 16; i++ ) {
+    nn->layer0.weights()( i ) = 0;
+  }
+  nn->layer0.biases()( 0 ) = 0;
+  */
+  float tempo = 50.0;
   float offset = 0;
-  for ( tempo = 80; tempo > 59; tempo-- ) {
+  for( int run = 0; run < 2000; run++){
+  for ( int tempo_int = 20; tempo_int < 250; tempo_int++ ) {
     /* test true function */
+    //float tempo_int_perturb = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/4));
+    //tempo = 60.0/(tempo_int + tempo_int_perturb);
+    tempo = 60.0/tempo_int;
     int i = 0;
     while ( true ) {
-      if ( i == 5 )
+      if ( i == 1 )
         break;
       i += 1;
+      //float rand_offset = 0;
+      float rand_offset = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/5));
       /* step 1: construct a unique problem instance */
-      Matrix<float, batch_size, input_size> input = gen_time( tempo, offset );
+      Matrix<float, batch_size, input_size> input = gen_time( tempo, offset + rand_offset );
 
       /* step 2: forward propagate and calculate loss functiom */
       nn->apply( input );
-      cout << "nn maps input: tempo: " << tempo << " offset " << offset << " => " << nn->output()( 0, 0 ) << endl;
+      //cout << "nn maps input: tempo: " << tempo << " offset " << offset + rand_offset << " => "
+      //     << nn->output()( 0, 0 ) << endl;
 
       /* step 3: backpropagate error */
       nn->computeDeltas();
@@ -116,7 +131,7 @@ void program_body( const string& midi_filename )
       nn->apply( input );
       auto loss_two_third_lr = loss_function( nn->output()( 0, 0 ), tempo );
 
-      cout << current_loss << " " << loss_four_third_lr << " " << loss_two_third_lr << endl;
+      //cout << current_loss << " " << loss_four_third_lr << " " << loss_two_third_lr << endl;
       auto min_loss = min( min( current_loss, loss_four_third_lr ), loss_two_third_lr );
       if ( min_loss == current_loss ) {
         learning_rate *= 2.0 / 3;
@@ -140,11 +155,11 @@ void program_body( const string& midi_filename )
         nn->layer0.biases()( 0 )
           = current_biase - two_third_lr * pd_loss_wrt_output * nn->getEvaluatedGradient( 0, 16 );
       }
-      cout << "weights: " << nn->layer0.weights() << endl;
-      cout << "biase: " << nn->layer0.biases()( 0 ) << endl;
+      //cout << "weights: " << nn->layer0.weights() << endl;
+      //cout << "biase: " << nn->layer0.biases()( 0 ) << endl;
     }
   }
-
+  }
   FileDescriptor piano { CheckSystemCall( midi_filename, open( midi_filename.c_str(), O_RDONLY ) ) };
   MidiProcessor midi_processor {};
   midi_processor.reset_time();
@@ -179,7 +194,7 @@ void program_body( const string& midi_filename )
             ret_mat( 15 ) = time_val;
           }
           nn->apply( ret_mat );
-          cout << nn->output()( 0, 0 ) << endl;
+          cout << "prediction: " << 60/(nn->output()( 0, 0 )) << endl;
           
         }
       }
@@ -190,9 +205,9 @@ void program_body( const string& midi_filename )
   while ( event_loop->wait_next_event( 5 ) != EventLoop::Result::Exit ) {
   }
 
-  
 
 }
+
 
 int main( int argc, char* argv[] )
 {
