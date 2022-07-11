10ac800b18053b1971e059cb22c521e3db49a13f
Nolan Miranda <mirandan@puffer5.stanford.edu>
2022-07-11 16:29:52 -0700

    printWeights in layer.hh now more accurate

--- src/nn/layer.hh
+++ src/nn/layer.hh
@@ -126,7 +126,7 @@ public:
   void printWeights( const unsigned int layer_num ) const
   {
     cout << layer_num << endl;
-    const IOFormat CleanFmt( 4, 0, ", ", "\n", "[", "]" );
+    const IOFormat CleanFmt( 10, 0, ", ", "\n", "[", "]" );
     cout << "weights:" << endl << weights_.format( CleanFmt ) << endl << endl;
 
     cout << "biases:" << endl << biases_.format( CleanFmt ) << endl << endl;
