b8dc28582f8fee374b0e213d23aa45a3d911ecd1
Grant Bishko <gbishko@stanford.edu>
2022-08-25 11:45:18 -0700

    with offset, not working as hoped yet but getting closer

--- src/frontend/grant_predict_tempo_midi_continuous.cc
+++ src/frontend/grant_predict_tempo_midi_continuous.cc
@@ -35,10 +35,16 @@ constexpr size_t layer_size3 = 30;
 constexpr size_t layer_size4 = 2560;
 constexpr float learning_rate = 0.000000001;
 
+// float CURR_TIME;
+
+float get_current_time(std::chrono::steady_clock::time_point base_time) {
+  return std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::steady_clock::now() - base_time ).count()/1000.0;
+}
 /* Create Input to NN -- Matrix of 16 timestamps (Notes) */
 Matrix<float, batch_size, input_size> calculate_input( deque<float> times, int num_notes, std::chrono::steady_clock::time_point base_time )
 {
-  float curr_time_secs = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::steady_clock::now() - base_time ).count()/1000.0;
+  float curr_time_secs = get_current_time(base_time);
+  // CURR_TIME = curr_time_secs;
 
   Matrix<float, batch_size, input_size> ret_mat;
   for ( size_t i = 0; i < input_size; i++ ) {
@@ -48,7 +54,7 @@ Matrix<float, batch_size, input_size> calculate_input( deque<float> times, int n
       ret_mat( i ) = curr_time_secs - times[i];
     }
   }
-  cout << "Timestamps: {" << ret_mat << "}" << endl;
+  //cout << "Timestamps: {" << ret_mat << "}" << endl;
   // cout << "Current time: " << curr_time_secs << endl;
   return ret_mat;
 }
@@ -57,11 +63,9 @@ Matrix<float, batch_size, input_size> calculate_input( deque<float> times, int n
 void nn_input_from_piano( const string& midi_filename) {
   
   /* Initialize Neural Network */
-  auto nn = make_unique<NeuralNetwork<float, num_layers, batch_size, input_size, output_size, 
-                                      layer_size1, layer_size2, layer_size3, layer_size4, 
-                                      output_size>>();
+  auto nn = make_unique<NeuralNetwork<float, num_layers, batch_size, input_size, output_size, layer_size1, layer_size2, layer_size3, layer_size4, output_size>>();
   nn->initialize(learning_rate);
-  string weights_file = "/home/emilypark/nnfun/src/frontend/grant_trial_weights.txt";
+  string weights_file = "/home/gbishko/nnfun/src/frontend/grant_trial_weights.txt";
   nn->init_params(weights_file);
 
   /* Set Up Piano */
@@ -72,8 +76,12 @@ void nn_input_from_piano( const string& midi_filename) {
   auto event_loop = make_shared<EventLoop>();
   deque<float> press_queue {};
   int num_notes = 0;
+  int num_pred_notes = 0;
 
-  Matrix<float, batch_size, input_size> ret_mat;
+  Matrix<float, batch_size, input_size> actual_mat;
+  deque<float> predicted_queue {};
+  Matrix<float, batch_size, input_size> pred_mat;
+  float time_val;
 
   /* rule #1: read events from MIDI piano */
   event_loop->add_rule( "read MIDI data", piano, Direction::In, [&] { midi_processor.read_from_fd( piano ); } );
@@ -83,7 +91,7 @@ void nn_input_from_piano( const string& midi_filename) {
       [&] {
       while ( midi_processor.has_event() ) {
           uint8_t event_type = midi_processor.get_event_type();
-          float time_val = midi_processor.pop_event() / 1.0;
+          time_val = midi_processor.pop_event() / 1.0;
           if ( event_type == 144 ) {
             if ( num_notes < 16 ) {
                 press_queue.push_front( time_val );
@@ -98,15 +106,48 @@ void nn_input_from_piano( const string& midi_filename) {
       [&] { return midi_processor.has_event(); } );
 
   /* Apply Neural Network */
-  while ( event_loop->wait_next_event( 5000 ) != EventLoop::Result::Exit ) {
-    ret_mat = calculate_input( press_queue, num_notes, base_time );
-    nn->apply_leaky( ret_mat );
+  while ( event_loop->wait_next_event( 50 ) != EventLoop::Result::Exit ) {
+    bool PLAY_NOTE = true;
+    actual_mat = calculate_input( press_queue, num_notes, base_time );
+    nn->apply_leaky( actual_mat );
     float next_timestamp = nn->get_output()( 0, 0 );   // in how many seconds the next note should be played (-)
-    float most_recent_timestamp = ret_mat[0];          
+    float most_recent_timestamp = actual_mat[0];          
     float seconds_left = most_recent_timestamp + next_timestamp;    // how many seconds left until the next note should be played
-    if ( seconds_left >= -0.05 && seconds_left <= 0.05) {
-      cout << "\nPLAY NOTE NOW\n" << endl;
+    
+    /* Predicted time is NOW (Play the note!) */
+    if ( seconds_left >= -0.05 && seconds_left <= 0.05 && PLAY_NOTE) {
+      float curr_time_secs = get_current_time(base_time);
+      cout << "\nPLAY NOTE NOW\n" << curr_time_secs << endl;
+      if ( predicted_queue.size() < 16 ) {
+          predicted_queue.push_front( curr_time_secs );
+          num_pred_notes++;
+      } else {
+          predicted_queue.push_front( curr_time_secs );
+          predicted_queue.pop_back();
+      }
+      // cout << "Predicted {";
+      // for (float elem : predicted_queue) cout << elem << " ";
+      // cout << "}" << endl;
+
+      PLAY_NOTE = false;
+
+      cout << "Actual: {" << actual_mat << "}" << endl;
+      cout << "Predicted: {" << pred_mat << "}" << endl;
+
+    } else {
+      PLAY_NOTE = true;
     }
+    pred_mat = calculate_input(predicted_queue, num_pred_notes, base_time);
+
+
+    // for ( size_t i = 0; i < input_size; i++ ) {
+    //   if ( i >= size_t(num_notes) ) {
+    //     pred_mat( i ) = 0;
+    //   } else {
+    //     pred_mat( i ) = CURR_TIME - predicted_queue[i];
+    //   }
+    // }
+
   }
 }
 
